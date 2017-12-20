1b1266b334b48f12249606525c4277e035f4686a
jluo117 <luojames52@gmail.com>
2017-12-19 16:08:03 -0800

    assets and updates

--- boardGame/ViewController.swift
+++ boardGame/ViewController.swift
@@ -9,9 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+   
+    @IBOutlet weak var start: UILabel!
+    var place = [UILabel]()
+    @IBOutlet weak var space2: UILabel!
+    
+    @IBOutlet weak var space3: UILabel!
+    
+    @IBOutlet weak var space4: UILabel!
+    
+    
+    @IBOutlet weak var space5: UILabel!
+    
+    @IBOutlet weak var space6: UILabel!
+    
+    @IBOutlet weak var space7: UILabel!
+    
+    @IBOutlet weak var space8: UILabel!
+    
+    
+    @IBOutlet weak var space9: UILabel!
+    
+    @IBOutlet weak var space10: UILabel!
+    
+    @IBOutlet weak var space11: UILabel!
+    
+    @IBOutlet weak var space12: UILabel!
+    
+    @IBOutlet weak var space13: UILabel!
+    
+    
+    @IBOutlet weak var space14: UILabel!
+    
+    
+    @IBOutlet weak var space15: UILabel!
+    
+    @IBOutlet weak var end: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        place = [start,space2,space3,space4,space5,space6,space7,space8,space9,space10,space11,space12,space13,space14,space15,end]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
